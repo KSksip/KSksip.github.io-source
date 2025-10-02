ff9a742c9ef2fcbb3dc03890a1fe62c6336c9a87
KSksip <->
2025-10-02 12:45:50 +0200

    started work on projects page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 
 import Home from '../routes/Home.vue'
+import Projects from '../routes/Projects.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', component: Home}
+    { path: '/', component: Home},
+    { path: '/projects', component: Projects }
   ],
 })
 
---
+++ src/routes/Projects.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const feature = ref();
+</script>
+
+<template>
+  <div>
+    <p>test</p>
+  </div>
+</template>
